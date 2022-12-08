78804d2b74e12425df69f4c1d3870520d29787d9
Elijah Edmondson <elijah.edmondson@gmail.com>
2022-12-08 09:50:04 -0500

    Create Create ppt files.R

---
+++ Create ppt files.R
@@ -0,0 +1,38 @@
+
+library(officer)
+library(magrittr)
+library(flextable)
+
+study = "MHL 223128A"
+investigator = " Venturoni"
+path = "F:/QuPath/MHL 223128A Venturoni/export/"
+img.file <- list.files(path = "F:/QuPath/MHL 223128A Venturoni/export/", pattern = ".png", all.files = FALSE,
+           full.names = TRUE)
+img.file.name <- list.files(path = "F:/QuPath/MHL 223128A Venturoni/export/", pattern = ".png", all.files = FALSE,
+                            full.names = FALSE)
+
+number <- length(img.file)
+doc <- read_pptx()
+doc <- add_slide(doc, layout = "Title and Content")#, master = "Office Theme")
+doc <- ph_with(doc, value = study, location = ph_location_type(type = "title"))
+doc <- ph_with(doc, value = format(Sys.Date()), location = ph_location_type(type = "dt"))
+doc <- ph_with(doc, value = "Manifest", location = ph_location_type(type = "sldNum"))
+
+Manifest <- list.files(path = path, pattern = "-a.png", all.files = FALSE, full.names = TRUE)
+Manifest <- as.data.frame(Manifest)
+set_flextable_defaults(font.size = 8, theme_fun = theme_booktabs, padding = 1)
+ft <- flextable(Manifest)
+ft <- autofit(ft)
+doc <- ph_with(x = doc, ft, location = ph_location_type(type = "body") )
+
+for(i in 1:number) {
+    doc <- add_slide(doc)
+    doc <- ph_with(doc, value = paste(study), location = ph_location_type(type = "title"))
+    doc <- ph_with(doc, value = paste(img.file.name[i]), location = ph_location_type(type = "ftr"))
+    doc <- ph_with(doc, value = format(Sys.Date()), location = ph_location_type(type = "dt"))
+    doc <- ph_with(doc, value = paste("slide",i), location = ph_location_type(type = "sldNum"))
+    doc <- ph_with(x = doc, external_img(img.file[i]), location = ph_location_type(type = "body"), use_loc_size = TRUE)
+}
+
+
+print(doc, target = paste0("C:/Users/edmondsonef/Desktop/",study,investigator,".pptx"))
